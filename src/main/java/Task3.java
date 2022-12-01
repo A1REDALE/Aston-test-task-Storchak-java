1e323d42c5b060a0f33485d1d9b8e64c22127a81
Evgeniy Storchak <EStorchak@yandex.ru>
2022-12-01 21:45:04 +0300

    correction

--- src/main/java/Task3.java
+++ src/main/java/Task3.java
@@ -7,7 +7,7 @@ public class Task3 {
         nums[i] = new Random().nextInt(100);
     }
         for (int num : nums) {
-            System.out.print("[" + num +"]");
+            System.out.print(num + " ");
         }
         for (int num : nums) {
             if (num % 3 == 0 && num > 0) {
