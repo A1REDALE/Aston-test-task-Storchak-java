fb2fce067834ff241ef418e2f3bbce61841327b8
Evgeniy Storchak <EStorchak@yandex.ru>
2022-12-04 11:22:25 +0300

    del class

--- src/main/java/Null.java
+++
@@ -1,9 +0,0 @@
-public class Null {
-    public static void main(String[] args) {
-        int [] nums = {0,1,2,3,4,5,6,0};
-        for(int num : nums)
-        if(num % 3 == 0){
-            System.out.println(num);
-        }
-    }
-}
